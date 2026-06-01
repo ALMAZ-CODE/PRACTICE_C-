b3e2e87dfb6bc8c62eab856107591dec706bbb22
ALMAZ CODE <almashassan2006@gmail.com>
2026-06-01 22:50:52 +0530

    arthematic pattern

---
+++ code.cpp
@@ -0,0 +1,19 @@
+//pyramid pattern 
+#include <iostream>
+using namespace std;
+int main(){
+int (n);
+cout<<"Enter you number: ";
+cin>> n;
+for (int i = 0;i<n;i++){
+    for(int j=0;n>j;j++){
+        cout<<"* ";
+    }
+    for(int k =0;n<k;k++ ){
+    cout<<" ";
+    }
+    cout<<"\n";
+
+}
+    return 0;
+}
