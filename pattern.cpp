41bdf63322ad287cb34f19684e0d318a5399ca12
ALMAZ CODE <almashassan2006@gmail.com>
2026-06-02 22:09:13 +0530

    step-pyramid

--- pattern.cpp
+++ pattern.cpp
@@ -5,11 +5,16 @@ int main(){
 cout<<"Enter your number: ";
 cin>>n;
 for(int i =0;n>i;i++){
+    int num =n;
     for(int j =1;j<i+2;j++){
-        cout<<j<<" ";}
+        cout<<j;}
     for(int k =0;k<n-i;k++){
-        cout<<" ";
+        cout<<" ";}
+    for(int l=0,i>l;l++){
+        cout<<num;
+        num--;
     }
+cout<<"\n"
 }
 return 0;
 }
