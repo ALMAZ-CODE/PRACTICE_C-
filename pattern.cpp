a4413ee7a604a534ab6ef483cc7f408597ac4545
ALMAZ CODE <almashassan2006@gmail.com>
2026-06-02 13:22:14 +0530

    pyramid

---
+++ pattern.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+int (n);
+cout<<"Enter you number: ";
+cin>> n;
+for (int i =0;n>i;i++){
+    for(int j = 1;i+2>j;j++){
+    cout<<j<<" ";
+} 
+cout <<"\n";}
+
+
+
+return 0;}
