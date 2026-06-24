ae43385cc197c1055c5ccc25abf2b0ce5ebc0987
ALMAZ CODE <almashassan2006@gmail.com>
2026-06-24 12:34:57 +0530

    DecBin & BinDec

--- code.cpp
+++ code.cpp
@@ -1,19 +1,33 @@
-//pyramid pattern 
+// DECIMAL TO BINARY 
 #include <iostream>
 using namespace std;
+DecBin(int n){
+    int num = 0;
+    int pow = 1;
+    for(int i=0;n>0;i++){
+    int rem = n % 2 ;
+    n /= 2;
+    num =num +(rem*pow) ;
+    pow*= 10;
+}return num;}
+
+BinDec(int n){
+    int num = 0, pow = 1;
+    for(int i = 0;n>0;i++){
+        int rem = n%10;
+        n /= 10;
+        num = num +(rem*pow);
+        pow*= 2;
+    }
+return num;
+}
+
+
+
 int main(){
 int (n);
 cout<<"Enter you number: ";
 cin>> n;
-for (int i = 0;i<n;i++){
-    for(int j=0;n>j;j++){
-        cout<<"* ";
-    }
-    for(int k =0;n<k;k++ ){
-    cout<<" ";
-    }
-    cout<<"\n";
-
-}
+cout<<"Binary to Decimal : "<<BinDec(n)<<endl;
     return 0;
 }
